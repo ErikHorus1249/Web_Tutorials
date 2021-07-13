df97e2b9eb1d958d58e15db2e015aee8f6cc0c02
Việt Nguyễn Hoàng <hoangvietnguyen1105@gmail.com>
2021-07-13 20:02:41 +0700

    function

---
+++ basic_javascipt/lesson_6/function/main.js
@@ -0,0 +1,20 @@
+
+
+
+// [BÀI TẬP]Viết một chương trình cho phép nhập vào hai số và kết quả trả về bằng alert là kết quả của phép cộng
+// của hai số đó
+// CHÚ Ý : viết một function để thực hiện bài toán
+
+// dầu vào là gì ? 5 , 4
+// xử lý cái gì ? thực hiện phép cộng : 5 + 4 
+// kết quả là gì ? 9
+
+function add(num1, num2){
+    var kq = num1 + num2
+    return kq 
+}
+
+so1 = Number(prompt("So thu nhat"))
+so2 = Number(prompt("So thu hai"))
+
+alert(add(so1,so2))
